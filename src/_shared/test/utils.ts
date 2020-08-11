067cb65068988ba5e9521f15470d3d6bcc539559
nenjotsu <renenjo.valente@gmail.com>
2020-08-12 00:05:42 +0800

    initial commit

---
+++ src/_shared/test/utils.ts
@@ -0,0 +1,30 @@
+import m from 'mithril';
+export const TIMEOUT = 70;
+
+export function hasClass(el: HTMLElement, className: string) {
+  return el.classList.contains(className);
+}
+
+export function hasChildClass(el: HTMLElement, className: string) {
+  return el.querySelector(`.${className}`);
+}
+
+export function triggerEvent(el: Element, type: string, callback: Function) {
+  setTimeout(() => {
+    el.dispatchEvent(new Event(type, { bubbles: true }));
+
+    setTimeout(callback, TIMEOUT);
+  }, TIMEOUT);
+}
+
+export function timeoutRedraw(fn: Function, done: Function) {
+  setTimeout(() => {
+    fn();
+    m.redraw();
+    setTimeout(done, TIMEOUT);
+  }, TIMEOUT);
+}
+
+export function keyboardEvent(el: HTMLElement, key: number) {
+  el.dispatchEvent(new KeyboardEvent('keydown', { which: key, bubbles: true } as any));
+}
